3e5c76398543633654f728c3e82fcabd25561cbd
Sebastian Marcet <smarcet@gmail.com>
2018-07-12 04:00:30 -0300

    [smarcet] * migrated to send grid api

--- api/cron_jobs/mail_job.py
+++ api/cron_jobs/mail_job.py
@@ -1,7 +1,12 @@
 from django_cron import CronJobBase, Schedule
+
+from backend import settings
 from ..models import MailRequest
 from django.core.mail import send_mail
 
+import sendgrid
+import os
+from sendgrid.helpers.mail import *
 
 class MailCronJob(CronJobBase):
     RUN_EVERY_MINS: int = 1  # every minute
@@ -12,14 +17,18 @@ class MailCronJob(CronJobBase):
     def do(self):
         mails_2_send = MailRequest.objects.filter(is_sent=False)
         for mail_2_send in mails_2_send:
+            print("sending email to {}".format(mail_2_send.to_address))
 
-            send_mail(
-                subject=mail_2_send.subject,
-                message=mail_2_send.body,
-                from_email=mail_2_send.from_address,
-                recipient_list=[mail_2_send.to_address, ],
-                html_message=mail_2_send.body
-            )
+            sg = sendgrid.SendGridAPIClient(apikey=settings.SEND_GRID_API_KEY)
+            from_email = Email(mail_2_send.from_address)
+            to_email = Email(mail_2_send.to_address)
+            subject = mail_2_send.subject
+            content = Content("text/plain", mail_2_send.body)
+            mail = Mail(from_email, subject, to_email, content)
+            response = sg.client.mail.send.post(request_body=mail.get())
+            print(response.status_code)
+            print(response.body)
+            print(response.headers)
 
             mail_2_send.mark_as_sent()
             mail_2_send.save(force_update=True)
--- backend/settings.py
+++ backend/settings.py
@@ -211,6 +211,8 @@ CRON_CLASSES = [
 
 MACADDRESS_DEFAULT_DIALECT = 'netaddr.mac_eui48'
 
+SEND_GRID_API_KEY = "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"
+
 #DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
 #GS_BUCKET_NAME = 'psqcom'
 #GS_PROJECT_ID = 'psq-api-200120'
