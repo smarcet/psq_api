b6e9e09c4f1c9bf8fdf3ee9e0c82e340bec5707b
Sebastian Marcet <smarcet@gmail.com>
2018-08-22 01:52:08 -0300

    [smarcet] * bug fixing

--- api/cron_jobs/mail_job.py
+++ api/cron_jobs/mail_job.py
@@ -18,41 +18,45 @@ class MailCronJob(CronJobBase):
     @transaction.atomic
     def do(self):
         logger = logging.getLogger('cronjobs')
-
         BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         pid_file = os.path.join(BASE_DIR, "run/{pid}.pid".format(pid=self.code))
-        if os.path.exists(pid_file):
-            logger.info("MailCronJob - is already running, skipping it")
-            return
 
-        open(pid_file, 'w').close()
+        try:
 
-        mails_2_send = MailRequest.objects.filter(is_sent=False)
-        for mail_2_send in mails_2_send:
-            try:
-                logger.info("sending email to {}".format(mail_2_send.to_address))
+            if os.path.exists(pid_file):
+                logger.info("MailCronJob - is already running, skipping it")
+                return
 
-                sg = sendgrid.SendGridAPIClient(apikey=settings.SEND_GRID_API_KEY)
-                from_email = Email(mail_2_send.from_address)
+            logger.info("running MailCronJob")
+            open(pid_file, 'w').close()
 
-                if settings.DEBUG:
-                    to_email = Email(settings.DEBUG_EMAIL)
-                else:
-                    to_email = Email(mail_2_send.to_address)
+            mails_2_send = MailRequest.objects.filter(is_sent=False)
+            for mail_2_send in mails_2_send:
+                try:
+                    logger.info("sending email to {}".format(mail_2_send.to_address))
 
-                subject = mail_2_send.subject
-                content = Content("text/html", mail_2_send.body)
-                mail = Mail(from_email, subject, to_email, content)
-                response = sg.client.mail.send.post(request_body=mail.get())
-                print(response.status_code)
-                print(response.body)
-                print(response.headers)
+                    sg = sendgrid.SendGridAPIClient(apikey=settings.SEND_GRID_API_KEY)
+                    from_email = Email(mail_2_send.from_address)
 
-                mail_2_send.mark_as_sent()
-                mail_2_send.save(force_update=True)
+                    if settings.DEBUG:
+                        to_email = Email(settings.DEBUG_EMAIL)
+                    else:
+                        to_email = Email(mail_2_send.to_address)
 
-            except Exception as exc:
-                logger.error("MailCronJob - error", exc)
+                    subject = mail_2_send.subject
+                    content = Content("text/html", mail_2_send.body)
+                    mail = Mail(from_email, subject, to_email, content)
+                    response = sg.client.mail.send.post(request_body=mail.get())
+                    print(response.status_code)
+                    print(response.body)
+                    print(response.headers)
 
-        if os.path.exists(pid_file):
-            os.remove(pid_file)
+                    mail_2_send.mark_as_sent()
+                    mail_2_send.save(force_update=True)
+
+                except Exception as exc:
+                    logger.error("MailCronJob - error", exc)
+
+        finally:
+            if os.path.exists(pid_file):
+                os.remove(pid_file)
